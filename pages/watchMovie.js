edc22adb89969eedab5749c831b44e9f98d99198
jorgyamil <105092307+jorgyamil@users.noreply.github.com>
2022-08-30 16:11:02 -0500

    crack

--- pages/watchMovie.js
+++ pages/watchMovie.js
@@ -2,6 +2,7 @@ function AddON() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center md:flex-row">
       WooHoo! Lets go watch the movie.
+    Enviame un emoji de gato 😺	si llegaste hasta aca. 
     </div>
   );
 }
